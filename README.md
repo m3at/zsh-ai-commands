dcef746f559b9fa8a0163b28df736f64957f6a50
Paul Willot <kiwi.pwillot@gmail.com>
2025-02-11 22:34:28 +0900

    Use gemini, simplify a bit, update readme

--- README.md
+++ README.md
@@ -1,37 +1,33 @@
+Forked from [muePatrick](https://github.com/muePatrick/zsh-ai-commands) to use Gemini instead of OpenAI.
+
+---
+
 # ZSH AI Commands
 ![zsh-ai-commands-demo](./zsh-ai-commands-demo.gif)
 
-This plugin works by asking GPT (*gpt-4o*) for terminal commands that achieve the described target action.
+This plugin works by asking Gemini for terminal commands that achieve the described target action.
 
 To use it just type what you want to do (e.g. `list all files in this directory`) and hit the configured hotkey (default: `Ctrl+o`).
-When GPT responds with its suggestions just select the one from the list you want to use.
+When the model responds with its suggestions just select the one from the list you want to use.
 
 ## Requirements
 * [curl](https://curl.se/)
 * [fzf](https://github.com/junegunn/fzf)
   * note: you need a recent version of fzf (the apt version for example is fairly old and will not work)
+* [jq](https://github.com/jqlang/jq)
 * awk
 
 ## Installation
 
-### oh-my-zsh
-
-Clone the repository to you oh-my-zsh custom plugins folder:
-
-``` sh
-git clone https://github.com/muePatrick/zsh-ai-commands ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-ai-commands
+Using [zplug](https://github.com/zplug/zplug), add this line in your zshrc:
+```sh
+zplug "m3at/zsh-ai-commands"
 ```
 
-Enable it in your `.zshrc` by adding it to your plugin list:
+Export the API key in your by setting:
 
 ```
-plugins=(... zsh-ai-commands ...)
-```
-
-Set the API key in your by setting:
-
-```
-ZSH_AI_COMMANDS_OPENAI_API_KEY="sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ZSH_AI_COMMANDS_GEMINI_API_KEY="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 ```
 
 Replace the placeholder with your own key.
@@ -41,14 +37,13 @@ The config can be set e.g in your `.zshrc` in this case be careful to not leak t
 
 | Variable                                  | Default                                 | Description                                                                                                |
 | ----------------------------------------- | --------------------------------------- | ---------------------------------------------------------------------------------------------------------- |
-| `ZSH_AI_COMMANDS_OPENAI_API_KEY` | `-/-` (not set) | OpenAI API key |
+| `ZSH_AI_COMMANDS_GEMINI_API_KEY` | `-/-` (not set) | Google Gemini API key |
 | `ZSH_AI_COMMANDS_HOTKEY` | `'^o'` (Ctrl+o) | Hotkey to trigger the request |
-| `ZSH_AI_COMMANDS_LLM_NAME` | `gpt-4o` | LLM name |
-| `ZSH_AI_COMMANDS_N_GENERATIONS` | `5` | Number of completions to ask for |
-| `ZSH_AI_COMMANDS_EXPLAINER` | `true` | If true, GPT will comment the command |
+| `ZSH_AI_COMMANDS_LLM_NAME` | `gemini-2.0-flash-lite-preview-02-05` | LLM name |
+| `ZSH_AI_COMMANDS_N_GENERATIONS` | `3` | Number of completions to ask for |
 | `ZSH_AI_COMMANDS_HISTORY` | `false` | If true, save the natural language prompt to the shell history (and atuin if installed) |
 
 
 ## Known Bugs
 - [x] Sometimes the commands in the response have to much / unexpected special characters and the string is not preprocessed enough. In this case the fzf list stays empty.
-- [ ] The placeholder message, that should be shown while the GPT request is running, is not always shown. For me it only works if `zsh-autosuggestions` is enabled.
+- [ ] The placeholder message, that should be shown while the model request is running, is not always shown. For me it only works if `zsh-autosuggestions` is enabled.
--- zsh-ai-commands.zsh
+++ zsh-ai-commands.zsh
@@ -1,17 +1,18 @@
-#!/bin/zsh
+#!/usr/bin/zsh
 
 # Check if required tools are installed
 (( ! $+commands[fzf] )) && return
 (( ! $+commands[curl] )) && return
+(( ! $+commands[jq] )) && return
 
-# Check if if OpenAi API key ist set
-(( ! ${+ZSH_AI_COMMANDS_OPENAI_API_KEY} )) && echo "zsh-ai-commands::Error::No API key set in the env var ZSH_AI_COMMANDS_OPENAI_API_KEY. Plugin will not be loaded" && return
+# Check if if Gemini API key ist set
+(( ! ${+ZSH_AI_COMMANDS_GEMINI_API_KEY} )) && echo "zsh-ai-commands::Error::No API key set in the env var ZSH_AI_COMMANDS_GEMINI_API_KEY. Plugin will not be loaded" && return
 
 (( ! ${+ZSH_AI_COMMANDS_HOTKEY} )) && typeset -g ZSH_AI_COMMANDS_HOTKEY='^o'
 
-(( ! ${+ZSH_AI_COMMANDS_LLM_NAME} )) && typeset -g ZSH_AI_COMMANDS_LLM_NAME='gpt-4o'
+(( ! ${+ZSH_AI_COMMANDS_LLM_NAME} )) && typeset -g ZSH_AI_COMMANDS_LLM_NAME='gemini-2.0-flash-lite-preview-02-05'
 
-(( ! ${+ZSH_AI_COMMANDS_N_GENERATIONS} )) && typeset -g ZSH_AI_COMMANDS_N_GENERATIONS=5
+(( ! ${+ZSH_AI_COMMANDS_N_GENERATIONS} )) && typeset -g ZSH_AI_COMMANDS_N_GENERATIONS=3
 
 (( ! ${+ZSH_AI_COMMANDS_EXPLAINER} )) && typeset -g ZSH_AI_COMMANDS_EXPLAINER=true
 
@@ -39,104 +40,84 @@ fzf_ai_commands() {
   fi
 
   # FIXME: For some reason the buffer is only updated if zsh-autosuggestions is enabled
-  BUFFER="Asking $ZSH_AI_COMMANDS_LLM_NAME for a command to do: $ZSH_AI_COMMANDS_USER_QUERY. Please wait..."
+  # BUFFER="Asking $ZSH_AI_COMMANDS_LLM_NAME for a command to do: $ZSH_AI_COMMANDS_USER_QUERY. Please wait..."
   ZSH_AI_COMMANDS_USER_QUERY=$(echo "$ZSH_AI_COMMANDS_USER_QUERY" | sed 's/"/\\"/g')
   zle end-of-line
   zle reset-prompt
 
-  if [ $ZSH_AI_COMMANDS_EXPLAINER = true ]
-  then
-    ZSH_AI_COMMANDS_GPT_SYSTEM="You only answer 1 appropriate shell one liner that does what the user asks for. The command has to work with the $(basename $SHELL) terminal. Don't wrap your answer in code blocks or anything, dont acknowledge those rules, don't format your answer. Just reply the plaintext command. If your answer uses arguments or flags, you MUST end your shell command with a shell comment starting with ## with a ; separated list of concise explanations about each agument. Don't explain obvious placeholders like <ip> or <serverport> etc. Remember that your whole answer MUST remain a oneliner."
-    ZSH_AI_COMMANDS_GPT_EX="Description of what the command should do: 'list files, sort by descending size'. Give me the appropriate command."
-    ZSH_AI_COMMANDS_GPT_EX_REPLY="ls -lSr ## -l long listing ; -S sort by file size ; -r reverse order"
-    ZSH_AI_COMMANDS_GPT_USER="Description of what the command should do: '$ZSH_AI_COMMANDS_USER_QUERY'. Give me the appropriate command."
-      ZSH_AI_COMMANDS_GPT_REQUEST_BODY='{
-      "model": "'$ZSH_AI_COMMANDS_LLM_NAME'",
-      "n": '$ZSH_AI_COMMANDS_N_GENERATIONS',
-      "temperature": 1,
-      "messages": [
-        {
-          "role": "system",
-          "content": "'$ZSH_AI_COMMANDS_GPT_SYSTEM'"
-        },
-        {
-          "role": "user",
-          "content": "'$ZSH_AI_COMMANDS_GPT_EX'"
-        },
-        {
-          "role": "assistant",
-          "content": "'$ZSH_AI_COMMANDS_GPT_EX_REPLY'"
-        },
-        {
-          "role": "user",
-          "content": "'$ZSH_AI_COMMANDS_GPT_USER'"
+  ZSH_AI_COMMANDS_GPT_SYSTEM="You only answer 1 appropriate shell one liner that does what the user asks for. The command has to work with the $(basename $SHELL) terminal. Don't wrap your answer in code blocks or anything, dont acknowledge those rules, don't format your answer. Just reply the plaintext command. If your answer uses arguments or flags, you MUST end your shell command with a shell comment starting with ## with a ; separated list of concise explanations about each agument. Don't explain obvious placeholders like <ip> or <serverport> etc. Remember that your whole answer MUST remain a oneliner."
+  ZSH_AI_COMMANDS_GPT_EX="Description of what the command should do: 'list files, sort by descending size'. Give me the appropriate command."
+  ZSH_AI_COMMANDS_GPT_EX_REPLY="ls -lSr ## -l long listing ; -S sort by file size ; -r reverse order"
+  ZSH_AI_COMMANDS_GPT_USER="Description of what the command should do: '$ZSH_AI_COMMANDS_USER_QUERY'. Give me the appropriate command."
+  ZSH_AI_COMMANDS_GPT_REQUEST_BODY='{
+    "system_instruction": {
+      "parts": {
+        "text": "'$ZSH_AI_COMMANDS_GPT_SYSTEM'"
+      }
+    },
+    "contents": [
+      {
+        "role": "user",
+        "parts": {
+          "text": "'$ZSH_AI_COMMANDS_GPT_EX'"
         }
-      ]
-    }'
-  else
-    ZSH_AI_COMMANDS_GPT_SYSTEM="You only answer 1 appropriate shell one liner that does what the user asks for. The command has to work with the $(basename $SHELL) terminal. Don't wrap your answer in anything, dont acknowledge those rules, don't format your answer. Just reply the plaintext command."
-    ZSH_AI_COMMANDS_GPT_USER="Description of what the command should do:\n'''\n$ZSH_AI_COMMANDS_USER_QUERY\n'''\nGive me the appropriate command."
-    ZSH_AI_COMMANDS_GPT_REQUEST_BODY='{
-      "model": "'$ZSH_AI_COMMANDS_LLM_NAME'",
-      "n": '$ZSH_AI_COMMANDS_N_GENERATIONS',
-      "temperature": 1,
-      "messages": [
-        {
-          "role": "system",
-          "content": "'$ZSH_AI_COMMANDS_GPT_SYSTEM'"
-        },
-        {
-          "role": "user",
-          "content": "'$ZSH_AI_COMMANDS_GPT_USER'"
+      },
+      {
+        "role": "model",
+        "parts": {
+          "text": "'$ZSH_AI_COMMANDS_GPT_EX_REPLY'"
         }
-      ]
-    }'
-  fi
+      },
+      {
+        "role": "user",
+        "parts": {
+          "text": "'$ZSH_AI_COMMANDS_GPT_USER'"
+        }
+      }
+    ],
+    "generationConfig": {
+      "candidateCount": '$ZSH_AI_COMMANDS_N_GENERATIONS',
+      "maxOutputTokens": 128,
+      "temperature": 1
+    }
+  }'
 
   # check request is valid json
   {echo "$ZSH_AI_COMMANDS_GPT_REQUEST_BODY" | jq > /dev/null} || {echo "Couldn't parse the body request" ; return}
 
-  ZSH_AI_COMMANDS_GPT_RESPONSE=$(curl -q --silent https://api.openai.com/v1/chat/completions \
+  MODEL_NAME=$ZSH_AI_COMMANDS_LLM_NAME
+  curl --silent "https://generativelanguage.googleapis.com/v1beta/models/"$MODEL_NAME":generateContent?key=$ZSH_AI_COMMANDS_GEMINI_API_KEY" \
     -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $ZSH_AI_COMMANDS_OPENAI_API_KEY" \
-    -d "$ZSH_AI_COMMANDS_GPT_REQUEST_BODY")
+    -H "Accept: application/json" \
+    -d "$ZSH_AI_COMMANDS_GPT_REQUEST_BODY" > /tmp/zshllmresp.json
   local ret=$?
 
   # if the json parsing fails, retry after some formating
-  exit_code=$(echo "$ZSH_AI_COMMANDS_GPT_RESPONSE" | jq -r '.choices[].message.content' 2>&1) || exit_code=""
+  exit_code=$(jq -r '.candidates[0].content.parts[0].text' /tmp/zshllmresp.json 2>&1) || exit_code=""
   if [ ! -z "$exit_code" ]
   then
-    ZSH_AI_COMMANDS_PARSED=$(echo "$ZSH_AI_COMMANDS_GPT_RESPONSE" |jq -r '.choices[].message.content' | uniq)
+    # ZSH_AI_COMMANDS_PARSED=$(jq -r '.candidates[].content.parts[0].text' /tmp/zshllmresp.json | uniq)
+    ZSH_AI_COMMANDS_PARSED=$(jq -r '.candidates[].content.parts[0].text | gsub("[\\n\\t]"; "")' /tmp/zshllmresp.json | uniq)
   else
-    # retrying with better parsing
-    exit_code=$(echo "$ZSH_AI_COMMANDS_GPT_RESPONSE" |sed '/"content": "/ s/\\/\\\\/g' | jq -r '.choices[].message.content' 2>&1) || exit_code=""
+    # retrying with better parsing - not really needed for gemini, but keep for consistency, and might help in some edge cases
+    # exit_code=$(echo "$ZSH_AI_COMMANDS_GPT_RESPONSE" |sed '/"text": "/ s/\\/\\\\/g' | jq -r '.candidates[0].content.parts[0].text' 2>&1) || exit_code=""
 
     if [ ! -z "$exit_code" ]
     then
         # parse output
-        ZSH_AI_COMMANDS_PARSED=$(echo "$ZSH_AI_COMMANDS_GPT_RESPONSE" |sed '/"content": "/ s/\\/\\\\/g' | jq -r '.choices[].message.content' | uniq)
+        # ZSH_AI_COMMANDS_PARSED=$(echo "$ZSH_AI_COMMANDS_GPT_RESPONSE" |sed '/"text": "/ s/\\/\\\\/g' | jq -r '.candidates[].content.parts[0].text' | uniq)
+        ZSH_AI_COMMANDS_PARSED=$(jq -r '.candidates[].content.parts[0].text | gsub("[\\n\\t]"; "")' /tmp/zshllmresp.json | uniq)
     else
         # give up parsing
-        echo "Failed to parse gpt response: $exit_code"
-        echo $ZSH_AI_COMMANDS_GPT_RESPONSE | jq -r '.choices[].message.content'
+        echo "Failed to parse gemini response: $exit_code"
+        # echo $ZSH_AI_COMMANDS_GPT_RESPONSE | jq -r '.candidates[].content.parts[0].text'
     fi
   fi
 
+  export ZSH_AI_COMMANDS_PARSED
 
-  if [ $ZSH_AI_COMMANDS_EXPLAINER = true ]
-  then
-    ZSH_AI_COMMANDS_SUGGESTIONS=$(echo $ZSH_AI_COMMANDS_PARSED | sort | awk -F ' *## ' '!seen[$1]++' -)
-
-    ZSH_AI_COMMANDS_SUGG_COMMANDS=$(echo $ZSH_AI_COMMANDS_SUGGESTIONS |  awk -F " ## " "{print \$1}")
-    ZSH_AI_COMMANDS_SUGG_COMMENTS=$(echo $ZSH_AI_COMMANDS_SUGGESTIONS |  awk -F " ## " "{print \$2}")
-
-    export ZSH_AI_COMMANDS_SUGG_COMMENTS  # otherwise fzf can't access it
-    ZSH_AI_COMMANDS_SELECTED=$(echo $ZSH_AI_COMMANDS_SUGG_COMMANDS | fzf --reverse --height=~100% --preview-window down:wrap --preview 'echo "$ZSH_AI_COMMANDS_SUGG_COMMENTS" | sed -n "$(({n}+1))"p | sed "s/;/\n/g" | sed "s/^\s*//g;s/\s*$//g"')
-
-  else
-    ZSH_AI_COMMANDS_SELECTED=$(echo $ZSH_AI_COMMANDS_PARSED | fzf --reverse --height=~100% --preview-window down:wrap --preview 'echo {}')
-  fi
-
+  selected=$(echo "$ZSH_AI_COMMANDS_PARSED" | sed 's/## /\t/g' | fzf --reverse --preview-window down:wrap --delimiter='\t' --with-nth=1 --preview 'echo {} | awk -F "\t" '\''{print $2}'\''')
+  ZSH_AI_COMMANDS_SELECTED=$(echo "$selected" | awk -F " \t" '{print $1}')
 
   # get the answers
   BUFFER=$ZSH_AI_COMMANDS_SELECTED
